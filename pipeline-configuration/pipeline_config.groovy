8cb6d672ed587c01f8b4208708e618c064285b24
Maarten Beeckmans <maartenb@inuits.eu>
2021-08-07 22:25:36 +0200

    remove stages

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -5,23 +5,3 @@ libraries{
   pulp
   puppet
 }
-
-stages {
-  validating {
-    bind_validate_zonefiles
-    puppet_validate_puppetcode
-    puppet_validate_hiera
-  }
-  packaging {
-    fpm_package_deb
-    fpm_package_rpm
-  }
-  deploying {
-    deploy_deb
-    deploy_rpm
-  }
-  mirroring {
-    pulp_upload_deb
-    pulp_upload_rpm
-  }
-}
